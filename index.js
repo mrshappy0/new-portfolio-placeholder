ad0d1db83a33a1db84d776ec0dc6655c903c340a
mrshappy0 <ajs11300@gmail.com>
2020-06-27 23:38:27 -0600

    fixed issue with background having a higher z-index than nav buttons

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function toggle(){
     let sec = document.getElementById('sec');
     sec.classList.toggle('active')
+
+    let nav = document.getElementById('navigation');
+    nav.classList.toggle('active')
 }
